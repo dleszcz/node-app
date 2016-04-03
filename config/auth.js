6aa341f39ba7f0c15b0c0c637b6dd4448e219633
daniel <danielxleszczynski@gmail.com>
2016-04-03 22:10:11 +0200

    google oauth

--- config/auth.js
+++ config/auth.js
@@ -3,5 +3,10 @@ module.exports = {
         'clientID' : '702894219851451',
         'clientSecret': 'edf8c1984b70debab8d45904d8a6a519',
         'callbackURL' : 'http://localhost:8080/auth/facebook/callback'
+    },
+    'googleAuth' : {
+        'clientID' : '519981354308-pegiqvh2mnttvum9l6uqss4qjagp5c6c.apps.googleusercontent.com',
+        'clientSecret': 'wiRr2Wd58RtUzqrsmoCQRdd6',
+        'callbackURL' : 'http://localhost:8080/auth/google/callback'
     }
 }
